06bdadabe217d7fd2098468f8de1fd69f680df22
Aurora <treibley95@gmail.com>
2025-09-27 06:44:13 -0600

    This is a basic working framework for an automated stock market with api for the agent to connect to and read the simulated market, this is still missing all of the rlml algorithm and some extra stock market features but this is a good first version

---
+++ action_handler.py
@@ -0,0 +1,33 @@
+import math
+import market_sim
+
+def max_buy(ticker,cash,data,day_id):
+    open = list(data['Open'][ticker])[day_id]
+    buy = cash/open
+    return buy
+
+
+
+def action_handler(actions, portfolio, stock_data, day_id, tickers):
+    #actions look like a list of all the stocks as a variable
+    #that variable is between -1 and 1
+
+    for action in actions:
+        ticker = tickers[actions.index(action)]
+        if action > 0:
+            total = max_buy(ticker,portfolio['cash'],stock_data,day_id)
+            buy_amount = action * total
+            math.floor(buy_amount)
+            if buy_amount > 0:
+                portfolio = market_sim.buy_request(buy_amount,portfolio,ticker,stock_data,day_id)
+            else:
+                continue
+        if action < 0:
+            total = portfolio['stocks'][ticker]
+            sell_amount = -action * total
+            math.floor(sell_amount)
+            if sell_amount > 0:
+                portfolio = market_sim.sell_request(sell_amount,portfolio,ticker,stock_data,day_id)
+            else:
+                continue
+    return portfolio
---
+++ market_sim.py
@@ -0,0 +1,34 @@
+from random import randint
+
+
+def buy_request(buy_amt,portfolio, ticker, data, day_id):
+    cash = portfolio['cash']
+    #the buy_amt is the amount the agent is requesting to buy
+    #we are going to randomize a prize based on the data, and then fufil the order if its possible
+    high = list(data['High'][ticker])[day_id]
+    low = list(data['Low'][ticker])[day_id]
+    buy_price = randint(low, high)
+    buy_order = buy_amt * buy_price
+    if buy_order <= cash:
+        cash -= buy_order
+        portfolio['stocks'][ticker][0] += buy_amt
+        portfolio['stocks'][ticker][1] += buy_order
+        return portfolio
+    else:
+        return portfolio
+
+
+def sell_request(sell_amt,portfolio, ticker, data, day_id):
+    cash = portfolio['cash']
+    high = list(data['High'][ticker])[day_id]
+    low = list(data['Low'][ticker])[day_id]
+    sell_price = randint(low, high)
+    sell_order = sell_amt * sell_price
+    if sell_order <= portfolio['stocks'][ticker][0]:
+        cash += sell_order
+        portfolio['stocks'][ticker][1] -= (portfolio['stocks'][ticker][1] / portfolio['stocks'][ticker][0]) * sell_amt
+        portfolio['stocks'][ticker][0] -= sell_amt
+
+        return portfolio
+    else:
+        return portfolio
