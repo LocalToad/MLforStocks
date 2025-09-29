c722bda8f09b23420d19bfb862dea5fb457d010d
Aurora <treibley95@gmail.com>
2025-09-29 01:53:15 -0600

    Sets up basic classes for the actor and the critic but i cant seem to figure out why i keep getting TypeError

--- rlmodel.py
+++ rlmodel.py
@@ -1,1 +1,74 @@
 #i know that this is empty dont mention it
+#Model environment
+#Feed Environment to Actor
+    #Actor Receives: 7n+1 inputs (n=stocks watched)
+      #For each stock being tracked(
+        #Stock Open Today
+        #Stock Close Yesterday
+        #Stock High Yesterday
+        #Stock Low Yesterday
+        #Stock Owned
+        #Stock Owned Static Value
+            #(value of stock when purchased * number of stock purchased)<- sum of all times stock has been bought divided by stock value
+            #this is generally displayed in the brokerage app
+        #)
+      #Cash Available
+#Actor takes a guess on actions
+    #Actor will have these outputs 2n outputs(n = stocks watched)
+        #For each stock being tracked(
+            #Stock sell
+            #Stock Buy
+        #)
+    #All outputs will be in range of 0-1 and will represent the probablity that the actor thinks an action will result in a good outcome
+#Feeds Environment and actions to Critic (9n+1 inputs n=stocks watched)
+    #Critic will see all the same data that the actor can but it can also see all of the outputs of the actor
+#Critic Takes a guess on loss
+    #Critic will output a single number, if it is 0 then it expects the price of the portfolio to go down
+        #if the number is greater than 0 than the critic expects the portfolio price to increase by that much
+#Actor affects environment
+    #
+#Calculate Difference between loss guess and real loss
+#Critic learns from mistake
+#Critic tells Actor how bad it sucks
+#Actor Learns from mistake
+import tf_keras
+from tf_keras.models import Sequential, load_model
+from tf_keras.layers import Dense
+
+class actor(tf_keras.layers):
+
+    def __init__(self, stocks,layer1):
+        super().__init__()
+        input_length = (7*stocks)+1
+        output_length = 2*stocks
+        self.model = Sequential()
+        self.model.add(tf_keras.layers.Dense(units=layer1, activation='leaky_relu', input_dim=input_length))
+        self.model.add(tf_keras.layers.Dense(units=layer1, activation='leaky_relu'))
+        self.model.add(tf_keras.layers.Dense(units=output_length, activation='softmax'))
+
+    def call(self, inputs):
+        x = self.model.output(inputs)
+
+    def summary(self):
+        self.model.summary()
+
+
+class critic(tf_keras.layers):
+    def __init__(self, stocks,layer1):
+        super().__init__()
+        input_length = (9*stocks)+1
+        self.model = Sequential()
+        self.model.add(tf_keras.layers.Dense(units=layer1, activation='leaky_relu', input_dim=input_length))
+        self.model.add(tf_keras.layers.Dense(units=layer1, activation='leaky_relu'))
+        self.model.add(tf_keras.layers.Dense(units=1, activation='leaky_relu'))
+
+    def call(self, inputs):
+        return self.model.output(inputs)
+
+    def summary(self):
+        self.model.summary()
+
+a = actor(1,16)
+c = critic(1,16)
+a.summary()
+c.summary()
